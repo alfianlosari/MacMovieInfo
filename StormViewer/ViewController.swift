3d6bc963fdc05253295a1192ef94ccd52bbf8408
Alfian Losari <alfianlosari@gmail.com>
2018-11-29 06:52:57 +0700

    Initial Commit

---
+++ StormViewer/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  StormViewer
+//
+//  Created by Alfian Losari on 11/29/18.
+//  Copyright © 2018 Alfian Losari. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
